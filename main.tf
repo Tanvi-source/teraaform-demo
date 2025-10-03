6fc5dd58a60812e4de1d99061cd4823a0cc08902
Tanvi-source <tanvibhusari2003@gmail.com>
2025-10-03 12:31:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "ubuntu_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "Terraform-Ubuntu-EC2"
   }
 }
