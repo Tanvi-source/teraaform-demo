ca2b612590189071ccf20af80f5e1ad367096cf4
Tanvi-source <tanvibhusari2003@gmail.com>
2025-10-03 12:38:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,5 +8,5 @@ variable "instance_type" {
 
 variable "ami_id" {
   # Ubuntu 22.04 LTS AMI in ap-south-1 (Mumbai). Update for other regions.
-  default = "ami-0dba2cb6798deb6d8"
+  default = "ami-02d26659fd82cf299"
 }
