019f4aa23888c2b5234c0d9ed332cfb6f00faaa8
Tanvi <tanvibhusari2003@gmail.com>
2025-10-03 10:54:32 +0530

    Initial commit - Terraform EC2 pipeline

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0dee22c13ea7a9a67"
+}
